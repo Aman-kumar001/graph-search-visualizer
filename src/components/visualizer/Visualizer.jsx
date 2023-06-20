4f47a39c48e9cae62d045fcb3041c5e6f32aa7da
Aman Kumar <amankum@maqsoftware.com>
2023-06-20 16:47:05 +0530

    grid created

--- src/components/visualizer/Visualizer.jsx
+++ src/components/visualizer/Visualizer.jsx
@@ -7,6 +7,10 @@ const Visualzer = () => {
 		cols: 20,
 		addBlockers: false,
 		reset: false,
+		start: false,
+		target: false,
+		startPos: [0, 0],
+		endPos: [21, 21],
 	});
 
 	const grid = () => {
@@ -18,7 +22,23 @@ const Visualzer = () => {
 						key={`${i}` + '-' + `${j}`}
 						row={i + 1}
 						col={j + 1}
-						className={styles.gridItem}
+						className={{
+							...styles.gridItem,
+							...(settings.startPos[0] === i + 1 &&
+							settings.startPos[1] === j + 1
+								? styles.startNode
+								: '' + ' ' + settings.endPos[0] === i + 1 &&
+								  settings.endPos[1] === j + 1
+								? styles.endNode
+								: ''),
+						}}
+						onClick={(e) => {
+							if (!settings.startPos) {
+								setSettings({ ...settings, startPos: [i + 1, j + 1] });
+							} else if (!settings.endPos) {
+								setSettings({ ...settings, endPos: [i + 1, j + 1] });
+							}
+						}}
 					></div>
 				);
 			}
@@ -29,13 +49,40 @@ const Visualzer = () => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.btnContainer}>
-				<Button variant='outlined' color='warning'>
+				<Button
+					variant='outlined'
+					color='warning'
+					onClick={() => {
+						setSettings({ ...settings, addBlockers: false });
+					}}
+				>
 					Reset
 				</Button>
 				<Button variant='outlined'>DFS</Button>
-				<Button variant='outlined'>BFS</Button>
-				<Button variant='outlined'>Dijkstra</Button>
-				<Button variant='outlined'>Add blockers</Button>
+				<Button
+					variant='outlined'
+					onClick={() => {
+						setSettings({ ...settings, addBlockers: false });
+					}}
+				>
+					BFS
+				</Button>
+				<Button
+					variant='outlined'
+					onClick={() => {
+						setSettings({ ...settings, addBlockers: false });
+					}}
+				>
+					Dijkstra
+				</Button>
+				<Button
+					variant={settings.addBlockers ? 'contained' : 'outlined'}
+					onClick={() => {
+						setSettings({ ...settings, addBlockers: !settings.addBlockers });
+					}}
+				>
+					Add blockers
+				</Button>
 			</div>
 
 			<div className={styles.gridCont}>{grid()}</div>
