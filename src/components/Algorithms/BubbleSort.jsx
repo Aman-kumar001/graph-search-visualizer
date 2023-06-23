3c476a7c0ba3fdb5ade62c19615e911e971b0768
Aman Kumar <2019200@iiitdmj.ac.in>
2023-06-24 00:28:19 +0530

    bubble sort complete

--- src/components/Algorithms/BubbleSort.jsx
+++ src/components/Algorithms/BubbleSort.jsx
@@ -1,9 +1,23 @@
+import { Render } from '../helpers/RenderArray';
+
 export async function BubbleSort(settings, setSettings) {
 	var array = settings.inputs;
 	for (var i = 0; i < array.length; i++) {
 		for (var j = 0; j < array.length - i - 1; j++) {
 			if (array[j] > array[j + 1]) {
+				Render(array[j], settings.rows, j, 'black');
+				Render(array[j + 1], settings.rows, j + 1, 'yellow');
+				await new Promise((resolve) => setTimeout(resolve, 25));
 				[array[j], array[j + 1]] = [array[j + 1], array[j]];
+				Render(array[j], settings.rows, j, 'red');
+				Render(array[j + 1], settings.rows, j + 1, 'red');
+				Render(settings.rows - array[j], settings.rows - array[j], j, 'none');
+				Render(
+					settings.rows - array[j + 1],
+					settings.rows - array[j + 1],
+					j + 1,
+					'none'
+				);
 			}
 		}
 	}
--- src/components/Algorithms/SelectionSort.jsx
+++ src/components/Algorithms/SelectionSort.jsx
@@ -5,13 +5,13 @@ export async function SelectionSort(settings, setSettings) {
 	for (var i = 0; i < array.length - 1; i++) {
 		for (var j = i + 1; j < array.length; j++) {
 			if (array[i] > array[j]) {
-				Render(array[i], settings.rows, i, 'yellow');
+				Render(array[i], settings.rows, i, 'black');
 				Render(array[j], settings.rows, j, 'yellow');
 				await new Promise((resolve) => setTimeout(resolve, 25));
 				[array[i], array[j]] = [array[j], array[i]];
 				Render(array[i], settings.rows, i, 'red');
 				Render(array[j], settings.rows, j, 'red');
-				Render(array[j] - array[i], settings.rows - array[i], i, 'none');
+				Render(settings.rows - array[j], settings.rows - array[j], j, 'none');
 			}
 		}
 	}
